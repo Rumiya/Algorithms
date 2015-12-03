7f6e485b50d6e57cd7a9eded7b0c62a3bba14764
Rumiya <rmurtazina@gmail.com>
2015-12-03 15:45:04 -0800

    Initial Commit

---
+++ Algorithms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorithms
+//
+//  Created by Rumiya Murtazina on 12/3/15.
+//  Copyright © 2015 abearablecode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
