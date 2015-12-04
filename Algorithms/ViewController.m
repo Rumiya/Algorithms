59ff3b9c1187b29efcb7d5faa402ee55c3935ecf
Rumiya <rmurtazina@gmail.com>
2015-12-03 21:03:39 -0800

    finding common elements in 3 arrays completed

--- Algorithms/ViewController.m
+++ Algorithms/ViewController.m
@@ -21,7 +21,8 @@
 
     // uncoment to run a method
     // [self sumZero];
-    [self reverseString];
+    // [self reverseString];
+    [self findElementsFromThreeArrays];
 }
 
 // Given an array of integers [-1,1,2,4,3] return true if there are 2 elements that sums 0
@@ -62,5 +63,33 @@
 
 }
 
+// Given 3 arrays: NSArray *a = [1,3,4,5]; NSArray *b = [-1,3,0,9]; NSArray *c = [0,31,32,22,6];
+// Find the elements from the arrays existing at least in 2 arrays.
+// Eg: [3, 0] Because 3 is presented in arrays a & b, and 0 in arrays b, c respectively.
+- (void)findElementsFromThreeArrays{
+    NSArray *aArray = [[NSArray alloc] initWithObjects: @1,@3,@4,@5,nil];
+    NSArray *bArray = [[NSArray alloc] initWithObjects: @-1,@3,@0,@9, nil];
+    NSArray *cArray = [[NSArray alloc] initWithObjects: @0,@31,@32,@22,@6, nil];
+
+    NSMutableArray *abcArray = [[NSMutableArray alloc] init];
+    [abcArray addObjectsFromArray:aArray];
+    [abcArray addObjectsFromArray:bArray];
+    [abcArray addObjectsFromArray:cArray];
+
+    NSCountedSet *abcCountSet = [[NSCountedSet alloc] initWithArray:abcArray];
+
+    NSMutableArray *resultArray = [[NSMutableArray alloc] init];
+
+
+    for (NSNumber *num in abcCountSet) {
+        if([abcCountSet countForObject:num]>1){
+            if (![resultArray containsObject:num]) {
+                [resultArray addObject:num];
+                NSLog(@"%@",num);
+            }
+        }
+    }
+
+}
 
 @end
